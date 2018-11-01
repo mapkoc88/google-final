a4f6f89496326f5d765e5d50fbdbc0ac7cd0c51e
mapkoc88 <mjc93@njit.edu>
2018-10-31 21:31:17 -0400

    Feature - Create Factory

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -4,6 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Car::class, function (Faker $faker) {
     return [
-        //
+        'make' => $faker->randomElement(['ford' ,'honda', 'toyota']),
+        'model' => $faker->company,
+        'year' => $faker->year,
     ];
 });
