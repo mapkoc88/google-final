0b2397114a95e4f39fa58cd3cb56ec8ce1c7a2cd
mapkoc88 <mjc93@njit.edu>
2018-11-02 16:13:06 -0400

    Feature - Test Unit-Updated User

---
+++ tests/Unit/UpdatedUserTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UpdatedUserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUpdatedUser()
+    {
+        $user = User::find(1);
+        $user->name = 'Steve Smith';
+        $user->save();
+        $name = $user->name;
+        $this->assertEquals($name,$user->name);
+    }
+}
