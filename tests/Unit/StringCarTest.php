7848a6c16b9b360506228a8dae538e46b667716a
mapkoc88 <mjc93@njit.edu>
2018-11-03 00:10:30 -0400

    Feature - Car Test String

---
+++ tests/Unit/StringCarTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class StringCarTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testStringCar()
+    {
+        $car =  Car::find(40);
+        $this->assertInternalType("string", $car->model);
+    }
+}
