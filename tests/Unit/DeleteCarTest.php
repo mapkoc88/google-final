5aa5d22f21c3a9a79c2376b0ccd974c33d15cb1f
mapkoc88 <mjc93@njit.edu>
2018-11-02 22:15:07 -0400

    Feature - Test Unit-Deleted Car Test

---
+++ tests/Unit/DeleteCarTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class DeleteCarTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testDeleteCar()
+    {
+        $car = Car::find(1);
+        $car->delete();
+        $this->assertTrue(true);
+    }
+}
